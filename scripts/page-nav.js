fbf3c93357c704a2273ca78f4d6b1a97530813fe
Robbie McKinstry <rem89@pitt.edu>
2016-01-24 17:58:26 -0500

    Bookmarking works.

--- scripts/page-nav.js
+++ scripts/page-nav.js
@@ -4,31 +4,3 @@ function rippleAndOpenNewWindow(linkLocation) {
         win.focus();
     }, 250);
 }
-
-function onLoad(selectedPageIndex) {
-    var pageNum = selectedPageIndex;
-    if (!(history.state === null)) {
-        pageNum = history.state.num;
-    }
-    if(!(window.location.pathname === "/")) {
-        // TODO: include a way to navigate to the page based on the URL
-    }
-    console.log(window.location.pathname);
-    document.querySelector('iron-pages').select(pageNum);
-}
-
-function onPopState(event) {
-    document.querySelector('iron-pages').select(event.state.num);
-}
-
-function selectPage() {
-    var pages = document.querySelector('iron-pages');
-    return function(num, title) {
-        var pageName = "Iron Page " + title;
-        history.pushState({ num: num, title: title}, pageName, title);
-        pages.select(num);
-    };
-}
-
-
-window.onpopstate = onPopState;
